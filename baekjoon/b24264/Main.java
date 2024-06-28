e6a5fb3df15e3e8f338bb7102cbdfca97ee5600d
차세대이커머스개발팀/김민준 <minjunk@hyundai-autoever.com>
2024-06-28 13:17:00 +0900

    백준: 24264 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ baekjoon/b24264/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.b24264;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println((long) N * (long) N);
+        System.out.println(2);
+    }
+}
