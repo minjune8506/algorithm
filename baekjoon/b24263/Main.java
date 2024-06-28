7963fb971e25d9487ac4fe1985491b3316e0d5d9
차세대이커머스개발팀/김민준 <minjunk@hyundai-autoever.com>
2024-06-28 11:14:01 +0900

    백준: 24263 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ baekjoon/b24263/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.b24263;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N);
+        System.out.println(1);
+    }
+
+}
