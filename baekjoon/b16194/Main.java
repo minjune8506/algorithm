ce2109a15c972e9b090674086a26c7a01d92c4be
Minjune <minjune8506@gmail.com>
2024-12-18 23:37:40 +0900

    [BOJ-16194] 카드 구매하기 2

---
+++ baekjoon/b16194/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.b16194;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] cards = new int[n + 1];
+        int[] dp = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+            dp[i] = cards[i];
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[j] + cards[i - j]);
+            }
+        }
+
+        System.out.print(dp[n]);
+    }
+}
